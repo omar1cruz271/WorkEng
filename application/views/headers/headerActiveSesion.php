f8b0fa6618ff5f78807e9c3de5c3d1d6c79eab2e
Omar1Cruz271 <omar1cruz2714721@gmail.com>
2019-11-25 19:50:08 -0600

    se compuso el direccionamiento

--- application/views/headers/headerActiveSesion.php
+++ application/views/headers/headerActiveSesion.php
@@ -41,13 +41,13 @@
             <div class="collapse navbar-collapse"
                 id="navcol-1">
                 <ul class="nav navbar-nav ml-auto">
-                    <li class="nav-item" role="presentation"><a class="nav-link active" href="<?php base_url(); ?>inicio">Inicio</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="<?php base_url(); ?>caracteristicas">Características</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="<?php base_url(); ?>postales">Empresas</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="<?php base_url();?>about">Acerca de</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="<?php base_url();?>contacto">Cont&aacute;ctanos</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="<?=base_url(); ?>inicio">Inicio</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="<?=base_url(); ?>caracteristicas">Características</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="<?=base_url(); ?>postales">Empresas</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="<?=base_url();?>about">Acerca de</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="<?=base_url();?>contacto">Cont&aacute;ctanos</a></li>
                     <li class="nav-item" role="presentation"><a class="nav-link" href="#"><?= $name ?></a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="<?php base_url();?>salir">Cerrar Sesi&oacute;n</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="<?=base_url();?>salir">Cerrar Sesi&oacute;n</a></li>
                 </ul>
             </div>
         </div>
