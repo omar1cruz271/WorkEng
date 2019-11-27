e47a169c4022cc0028c566c5fc7c0a5be42eee5d
VzqzAlfred <alfred.vzqz96@outlook.com>
2019-11-27 09:36:05 -0600

    cambio en registrio de admi

--- application/views/registroEmpresa.php
+++ application/views/registroEmpresa.php
@@ -8,7 +8,7 @@
                 </div>
                 <form id="formRegistro">
                     <div class="form-group"><label for="name">Nombe completo de la empresa</label><input class="form-control item" type="text" id="name" name="name" data-validetta="required,maxLength[9]"></div>
-                    <div class="form-group"><label for="name">Cédula de Identificación Fiscal/label><input class="form-control item" type="text" id="name" name="name" data-validetta="required,maxLength[64]"></div>
+                    <div class="form-group"><label for="name">Cédula de Identificación Fiscal< /label><input class="form-control item" type="text" id="name" name="name" data-validetta="required,maxLength[64]"></div>
                     <div class="form-group"><label for="password">Contrase&ntilde;a</label><input class="form-control item" type="password" id="password" name="password" data-validetta="required,minLength[5],maxLength[32]"></div>
                     <div class="form-group"><label for="email">Email</label><input class="form-control item" type="email" placeholder="ejemplo@dominio.com" id="email" name="email" data-validetta="required,email,maxLength[32]"></div>
                     <div class="form-group"><label for="mobile">M&oacute;vil</label><input class="form-control item" type="tel" id="mobile" name="mobile" data-validetta="required,number,minLength[10],maxLength[10]"></div>
